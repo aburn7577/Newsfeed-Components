f9f84d389742194143e1e15d799b1ac49c465a32
Ashley Burns <ashley@ashleys-mini.home>
2021-03-16 21:18:41 -0400

    Task 3 Menu is done

--- components/Menu.js
+++ components/Menu.js
@@ -31,3 +31,31 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+function menuMaker(array){
+  // creating element STEP 1
+  const menu = document.createElement('div')
+  const unordered = document.createElement('ul')
+  
+  // structure
+  menu.appendChild(unordered)
+  // adding class
+  menu.classList.add('menu')
+  // STEP 2
+  array.forEach(element => {
+    const list = document.createElement('li')  
+    unordered.appendChild(list)
+      list.textContent = element
+  });
+  // STEP 3
+  const menubutton = document.querySelector('.menu-button')
+// STEP 4 event listener
+menubutton.addEventListener('click', event =>{
+  menu.classList.toggle('menu--open')
+})
+// STEP 5 Return
+return menu
+}
+
+const headMenu = document.querySelector('.header')
+headMenu.appendChild(menuMaker(menuItems))
